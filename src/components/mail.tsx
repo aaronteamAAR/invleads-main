aff35a9fb057195e988ebab0de2dd1078bf445dd
Aaron Chris <rooneychris83@gmail.com>
2023-03-18 17:47:20 +0100

    chnaged version of few npms

--- src/components/mail.tsx
+++ src/components/mail.tsx
@@ -12,7 +12,7 @@ export default function MailForm() {
   }
 
 
-  const signUpSchema = Yup.object().shape({
+  const signUpSchema = Yup.object({
     fullName: Yup.string() 
     .min(3, 'Too short')
     .max(20, 'Too long')
@@ -23,6 +23,7 @@ export default function MailForm() {
     console.log(values);
   };
 
+ 
 
   return (
     <div>
@@ -30,56 +31,49 @@ export default function MailForm() {
       initialValues={{fullName: "", email: ""}} 
       onSubmit={onSubmit}
       validationSchema={signUpSchema}>
+             {(formik) => (
+        <Form className="w-64 bg-gray-50 p-4 flex flex-col shadow-sm">
+        <h2 className="text-center text-2xl font-bold">
+          Sign up for our newsletter!
+        </h2>
+       
 
-        <Form className="mx-auto flex w-full max-w-sm flex-col justify-center">
-          <div className="mb-6 md:flex md:items-center">
-            <div className="md:w-1/3">
-              <Label htmlFor="Email" text="Email" required={true} />
-            </div>
-            <div className="md:w-2/3">
-              <Field
-                id="email"
-                name="email"
-                type="email"
-                classNameName="w-full appearance-none rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:border-blue-500 focus:bg-white focus:outline-none"
-              />
-             <ErrorMessage name="email" component="div" className="text-red-700" />
-            </div>
-          </div>
-          <div className="mb-6 md:flex md:items-center">
-            <div className="md:w-1/3">
-             <Label htmlFor="FullName" text="Full-name" required={true} />
-            </div>
-            <div className="md:w-2/3">
-              <Field
-                id="fullName"
-                name="fullName"
-                classNameName="w-full appearance-none rounded border-2 border-gray-200 bg-white py-2 px-4 leading-tight text-gray-700 focus:border-blue-500 focus:bg-white focus:outline-none"
-              />
-                            <ErrorMessage name="fullName" component="div" className="text-red-700" />
-            </div>
-          </div>
-          {/* <div className="md:flex md:items-center mb-6">
-                      <div className="md:w-1/3"></div>
-                      <label className="md:w-2/3 block text-gray-500 font-bold">
-                        <input className="mr-2 leading-tight" type="checkbox" />
-                        <span className="text-sm" >
-                          Send me your newsletter!
-                        </span>
-                      </label>
-                    </div> */}
-          <div className="md:flex md:items-center">
-            <div className="md:w-1/3"></div>
-            <div className="md:w-2/3">
-              <button
-              className="my-2 px-4 py-2 bg-blue-700 disabled:opacity-50 transition-all duration-300"
-                type="submit"
-              >
-                Sign Up
-              </button>
-            </div>
-          </div>
-        </Form>
+        <div className="my-2 flex flex-col">
+          <Label text="Full-name" htmlFor="fullName" />
+          <Field
+            id="fullName"
+            className="p-2 border-2 border-gray-400"
+            name="lastName"
+          ></Field>
+          <ErrorMessage
+            component="div"
+            className="text-red-700"
+            name="fullName"
+          />
+        </div>
+
+        <div className="my-2 flex flex-col">
+          <Label text="Email" required={true} htmlFor="email" />
+          <Field
+            id="email"
+            className="p-2 border-2 border-gray-400"
+            name="email"
+          ></Field>
+          <ErrorMessage
+            component="div"
+            className="text-red-700"
+            name="email"
+          />
+        </div>
+        <button
+          disabled={!formik.isValid || !formik.dirty}
+          type="submit"
+          className="disabled:opacity-50 my-2 px-4 py-2 bg-blue-700 text-white transition-all duration-300"
+        >
+          Submit
+        </button>
+      </Form>
+        )}
       </Formik>
     </div>
   );
